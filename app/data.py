193e65357bba09a7b7be2ebf8bf3e5bce9dde645
Andrii Bilonozhko <31069156+dzendjo@users.noreply.github.com>
2021-03-10 13:43:03 +0200

    Update data.py

--- app/data.py
+++ app/data.py
@@ -11,8 +11,6 @@ jinja = Environment(loader=YamlLoader('templates.yml'))
 
 process_images = ['🌇', '🌆', '🏙']
 
-ai_api_key = '46d90da1-985c-44a9-9841-d7f45770df2f'
-
 improve_url = 'https://api.deepai.org/api/torch-srgan'
 colorize_url = 'https://api.deepai.org/api/colorizer'
 
